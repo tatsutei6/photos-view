dc177c6a8d472f1b1304280d24d72ee5cf7b10f3
tatsutei6 <tatsutei6@gmail.com>
2022-08-25 18:04:16 +0900

    remove vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <router-view></router-view>
+    <MobileHome v-if='isMobileDevice()' />
+    <PCHome v-else />
   </div>
 </template>
 
 <script setup>
-import { initREM, initTheme } from '@/utils'
+import { initREM, initTheme, isMobileDevice } from '@/utils'
+import MobileHome from '@/views/mobile-home/index.vue'
+import PCHome from '@/views/pc-home/index.vue'
+
 initREM()
 initTheme()
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import './styles/index.scss'
 import App from './App.vue'
-import router from '@/router'
 import store from './store'
 import 'virtual:svg-icons-register'
 import directive from './directives'
 
 
-createApp(App).use(router).use(store).use(directive).mount('#app')
+createApp(App).use(store).use(directive).mount('#app')
