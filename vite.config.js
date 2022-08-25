604182d79b430568ea3afb4606b70c86bff9be72
tatsutei6 <tatsutei6@gmail.com>
2022-08-25 17:42:12 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/photos-view/dist/',
   plugins: [vue(), DefineOptions(),
     createSvgIconsPlugin({
     // 指定需要缓存的图标文件夹
